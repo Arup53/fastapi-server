17d47c13a6a32abbb13dc01f4c0b80d427c5de1f
Arup53 <arupkumarhowlader12@gmail.com>
2025-04-02 13:23:45 +0600

    add fastapi data validation for db

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException, Depends
 from pydantic import BaseModel
+from typing import List,Annotated
 
 app= FastAPI() ;
 
 class User(BaseModel):
     name: str
     age : int
+
+class ChoiceBase(BaseModel):
+   choice_text = str 
+   is_correct = bool
+
+class QuestionBase(BaseModel):
+   question_text= str
+   choices: List[ChoiceBase]
+
 
 @app.get('/')
 async def home():
